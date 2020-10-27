829df1a9d80fac972ad099c6a8b3be471d4867af
atefeh97hmt <hemmati.atefeh97@gmail.com>
2020-10-27 18:43:16 +0330

    birthday list styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-
-
-
 function addRow() {
 
     var myName = document.getElementById("name");
@@ -12,9 +9,12 @@ function addRow() {
     var row = table.insertRow(rowCount);
 
     // row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
-    row.insertCell(0).innerHTML = myName.value;
-    row.insertCell(1).innerHTML = day.value;
-    row.insertCell(2).innerHTML = month.value;
+    row.insertCell(0).innerHTML = '<div type="text" value ="*"> ';
+    row.insertCell(1).innerHTML = myName.value;
+    row.insertCell(2).innerHTML = day.value;
+    row.insertCell(3).innerHTML = month.value;
+    row.insertCell(4).innerHTML = '<div type="text" value ="*"> ';
+    // row.insertCell(4).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
 
 }
 
