3261845102de9f933088baf33049fb4f417ca921
atefeh97hmt <hemmati.atefeh97@gmail.com>
2020-11-01 18:21:50 +0330

    class name changing

--- js/script.js
+++ js/script.js
@@ -68,12 +68,24 @@ function validate() {
 }
 
 
+function validate() {
+    isValid = true;
+    if (document.getElementById("name").value == "") {
+        isValid = false;
+        document.getElementById("name").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("name").classList.contains("hide"))
+            document.getElementById("name").classList.add("hide");
+    }
+    return isValid;
+}
 
 
 
 ////// search input filter//////
 
-function myFunction() {
+function SearchInput() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("filter");
     filter = input.value.toUpperCase();
@@ -97,107 +109,36 @@ function myFunction() {
 
 
 
-////// sort number//////
+//////sort number////
 
 
-birthdayList.onclick = function (e) {
+birthdayList.onclick = function(e) {
     if (e.target.tagName != 'TH') return;
 
     let th = e.target;
-
+ 
     sortlist(th.cellIndex, th.dataset.type);
-};
+  };
 
-function sortlist(colNum, type) {
+  function sortlist(colNum, type) {
     let tbody = birthdayList.querySelector('tbody');
 
     let rowsArray = Array.from(tbody.rows);
 
-    // compare(a, b) compares two rows, need for sorting
+
     let compare;
 
     switch (type) {
-        case 'number':
-            compare = function (rowA, rowB) {
-                return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
-            };
-            //     break;
-            //   case 'string':
-            //     compare = function(rowA, rowB) {
-            //       return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
-            //     };
-            break;
+      case 'number':
+        compare = function(rowA, rowB) {
+          return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+        };
+
+        break;
     }
 
     // sort
     rowsArray.sort(compare);
 
     tbody.append(...rowsArray);
-}
-
-
-
-
-
-
-
-// onclick="sortTable(2)"
-// function sortTable(n) { 
-//     var table; 
-//     table = document.getElementById("birthdayList"); 
-//     var rows, i, x, y, count = 0; 
-//     var switching = true; 
-
-//     // Order is set as ascending 
-//     var direction = "ascending"; 
-
-//     // Run loop until no switching is needed 
-//     while (switching) { 
-//         switching = false; 
-//         var rows = table.rows; 
-
-//         //Loop to go through all rows 
-//         for (i = 1; i < (rows.length - 1); i++) { 
-//             var Switch = false; 
-
-//             // Fetch 2 elements that need to be compared 
-//             x = rows[i].getElementsByTagName("TD")[n]; 
-//             y = rows[i + 1].getElementsByTagName("TD")[n]; 
-
-//             // Check the direction of order 
-//             if (direction == "ascending") { 
-
-//                 // Check if 2 rows need to be switched 
-//                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) 
-//                     { 
-//                     // If yes, mark Switch as needed and break loop 
-//                     Switch = true; 
-//                     break; 
-//                 } 
-//             } else if (direction == "descending") { 
-
-//                 // Check direction 
-//                 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) 
-//                     { 
-//                     // If yes, mark Switch as needed and break loop 
-//                     Switch = true; 
-//                     break; 
-//                 } 
-//             } 
-//         } 
-//         if (Switch) { 
-//             // Function to switch rows and mark switch as completed 
-//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]); 
-//             switching = true; 
-
-//             // Increase count for each switch 
-//             count++; 
-//         } else { 
-//             // Run while loop again for descending order 
-//             if (count == 0 && direction == "ascending") { 
-//                 direction = "descending"; 
-//                 switching = true; 
-//             } 
-//         } 
-//     } 
-// } 
+  }
